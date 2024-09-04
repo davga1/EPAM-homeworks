db3ce81f4b8f7fe47f82c9e5ef05db5327aff295
davgal <davvv.galstyan@gmail.com>
2024-09-04 13:59:08 +0400

    homework files

---
+++ Homework1/Print_One_By_One.java
@@ -0,0 +1,26 @@
+//Ենթադրենք ունենք քառանիշ թիվ, որը ստանումա ծրագիրը որպես մուտք։
+//Պետքա գրեք ծրագիր, որը կստանա ու կտպի տվյալ թվի բոլոր թվանշանները։
+//Ծրագիրը պետքա աշխատի ցանկացած քառանիշ թվի համար։
+
+package Homework1;
+
+import java.util.Scanner;
+
+public class Print_One_By_One {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number from 1000 to 9999");
+        int number = scanner.nextInt();
+        if (number < 1000 || number > 9999) {
+            System.out.println("Wrong number");
+        } else {
+            System.out.println("Number one:" + number % 10);
+            number = number / 10;
+            System.out.println("Number two:" + number % 10);
+            number = number / 10;
+            System.out.println("Number three:" + number % 10);
+            number = number / 10;
+            System.out.println("Number four:" + number % 10);
+        }
+    }
+}
