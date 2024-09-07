561ef3a4f1f7d4f173df7d6babf3fc293d1efe1b
davgal <davvv.galstyan@gmail.com>
2024-09-07 11:58:13 +0400

    Second Homework

---
+++ Homework2/IsTriangleOrNot.java
@@ -0,0 +1,26 @@
+//Որպես ծրագրի մուտք ստանում եք 3 հատ ամբողջ թիվ,
+// որոնք իրանցից պետքա ներկայացնեն պոտենցիալ եռանկյան կողմերի երկարություններ։
+// Ձեր խնդիրնա ստուգել թե արդյոք էդ թվերով հնարավորա ստանալ եռանկյուն ու տպել համապատասխան տեքստ՝
+// տեքստի մեջ նշելով նաև հենց թվերը, հակառակ դեպքում տպել եռանկյուն կառուցելու անհնարինության մասին տեքստ՝
+// էլի թվերը նշելով։
+
+package Homework2;
+
+import java.util.Scanner;
+
+public class IsTriangleOrNot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer value for side A");
+        int sideA = scanner.nextInt();
+        System.out.println("Enter an integer value for side B");
+        int sideB = scanner.nextInt();
+        System.out.println("Enter an integer value for side C");
+        int sideC = scanner.nextInt();
+        if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA) {
+            System.out.println("It is possible to make a triangle with sides " + sideA + "," + sideB + " and " + sideC);
+        } else {
+            System.out.println("It is not possible to make a triangle with sides " + sideA + "," + sideB + " and " + sideC);
+        }
+    }
+}
