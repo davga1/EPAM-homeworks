61be0f9316d924e50d16d2d9faea54d949e467ed
davgal <davvv.galstyan@gmail.com>
2024-09-19 10:53:21 +0400

    previous homeworks

---
+++ src/homework2/ReturnCash.java
@@ -0,0 +1,41 @@
+//Պատկերացրեք, սենց իրադրություն.
+// Կա ինչ-որ ժամանցային վայր,
+// որտեղ մուտքի գումարը 5000 դրամա ոչ vip հաճախորդների համար ու տարիքի մինիմալ սահմանափակումը 16 տարեկաննա.
+// հակառակ դեպքում ժամանցային վայր մուտք չկա ու ծրագիրը դադարումա աշխատանքը։
+// Հաճախորդի տարիքն ու vip հաճախորդ լինելու ճիշտ կամ սխալ լինելը մուտքագրվում են որպես input։
+// Հաջորդ քայլը պատվեր ձևակերպելնա. ձևակերպված պատվերի գումարը պետքա մուտքագրվի Ձեր ծրագրում ու զեղչվի 15%-ով,
+// եթե հաճախորդը vip-ա։
+// Ծրագրի վերջում տպեք, թե տվյալ հաճախորդը ընդհանուր ինչքան գումարա ծախսել տվյալ ժամանցային վայր մտնելուց հետո։
+
+package homework2;
+
+import java.util.Scanner;
+
+public class ReturnCash {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+        if (age < 16) {
+            System.out.println("Should be at least 16 years old to go in");
+            System.exit(6);
+        }
+        System.out.println("Are you VIP person?");
+        boolean isVip = scanner.nextBoolean();
+        System.out.println("How much money did you spend?");
+        int moneySpent = scanner.nextInt();
+        if(moneySpent <= 0){
+            System.out.println("Money spent cannot be negative number");
+            System.exit(2);
+        }
+        int sum;
+        if (isVip) {
+            sum = moneySpent - ((moneySpent * 15) / 100);
+            System.out.print("As a VIP person, you have 15% discount, your discount is (" + moneySpent + " * 15) / 100 = " + ((moneySpent * 15) / 100) + ", and your ");
+        } else {
+            System.out.print("You are not a VIP person, you have to pay additional 5000 to go in, your ");
+            sum = moneySpent + 5000;
+        }
+        System.out.println("final sum is " + sum);
+    }
+}
---
+++ src/homework3_arrays/ChangeArrayIndexes.java
@@ -0,0 +1,33 @@
+//Ներմուծում եք n ամբողջ թիվը, որին համապատասխան էլ պետքա սարքեք նոր զանգված։
+// Ներմուծում եք նաև թիվ՝ a։ Էս թիվը իրանից իրականում պետքա ներկայացնի տվյալ մասիվի անդամի ինդեքս։
+// Խնդիրը կայանումա նրանում, որ պետքա էդ ինդեքսից սկսած հաջորդ 3 անդամները ստանան հենց իրանց ինդեքսների արժեքները։
+// Օրինակ եթե 5-ա ներմուծվել, պետքա 6-րդի արժեքը լինի 6 7-րդինը 7, ու 8-րդինը՝ 8։
+// Եթե ներմուծված տվյալների պատճառով հնարավոր չի խնդիրը կատարել, ուրեմն ծրագիրը պետքա համապատասխան հաղորդագրությամբ դադարացնի աշխատանքը։
+
+package homework3_arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ChangeArrayIndexes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your array length");
+        int length = scanner.nextInt();
+        if (length < 0) {
+            System.out.println("Array length cannot be negative");
+            System.exit(666);
+        }
+        int[] array = new int[length];
+        System.out.println("Enter a number");
+        int number = scanner.nextInt();
+        if (number + 3 >= array.length) {
+            System.out.println("Provided number is greater than it can be"); // I don't know what can I write here, any ideas?
+            System.exit(6);
+        }
+        array[number + 1] = number + 1;
+        array[number + 2] = number + 2;
+        array[number + 3] = number + 3;
+        System.out.println(Arrays.toString(array));
+    }
+}
