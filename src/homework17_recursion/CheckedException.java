697a6190fc215e894bd0d30df7446adc88579582
davgal <davvv.galstyan@gmail.com>
2024-12-10 19:06:17 +0400

    changed extendede Exception name

--- src/homework17_recursion/CheckedException.java
+++ src/homework17_recursion/CheckedException.java
@@ -1,6 +1,6 @@
 package homework17_recursion;
 
-public class CheckedException extends RuntimeException {
+public class CheckedException extends Exception {
     public CheckedException(String message) {
         super(message);
     }
