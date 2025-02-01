58554fabd7452b9079929c6ff3321e7044bc230d
davgal <davvv.galstyan@gmail.com>
2025-02-01 12:40:27 +0400

    little changes

--- src/homework20_maps/Main.java
+++ src/homework20_maps/Main.java
@@ -15,18 +15,17 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
+        Building building1 = new Building(2, 0, new ArrayList<House>(List.of(Houses.h1, Houses.h2)));
+        Building building2 = new Building(3, 5, new ArrayList<House>(List.of(Houses.h3, Houses.h4, Houses.h5)));
+        Building building3 = new Building(1, 1, new ArrayList<House>(List.of(Houses.h1, Houses.h5, Houses.h9)));
+        Building building4 = new Building(3, 2, new ArrayList<House>(List.of(Houses.h6, Houses.h8, Houses.h10)));
+        Complex complex1 = new Complex(new ArrayList<>(List.of(building1, building2)));
+        Complex complex2 = new Complex(new ArrayList<>(List.of(building3, building4)));
+        Complex complex3 = new Complex(new ArrayList<>(List.of(building3, building4)));
+        HashMap<Complex, String> complexesMap = new HashMap<>();
+        complexesMap.put(complex1, "Avangard 4");
+        complexesMap.put(complex2, "Yolyan 1");
         try {
-            Building building1 = new Building(2, 0, new ArrayList<House>(List.of(Houses.h1, Houses.h2)));
-            Building building2 = new Building(3, 5, new ArrayList<House>(List.of(Houses.h3, Houses.h4, Houses.h5)));
-            Building building3 = new Building(1, 1, new ArrayList<House>(List.of(Houses.h1, Houses.h5, Houses.h9)));
-            Building building4 = new Building(3, 2, new ArrayList<House>(List.of(Houses.h6, Houses.h8, Houses.h10)));
-            Complex complex1 = new Complex(new ArrayList<>(List.of(building1, building2)));
-            Complex complex2 = new Complex(new ArrayList<>(List.of(building3, building4)));
-            Complex complex3 = new Complex(new ArrayList<>(List.of(building3, building4)));
-            HashMap<Complex, String> complexesMap = new HashMap<>();
-            complexesMap.put(complex1, "Avangard 4");
-            complexesMap.put(complex2, "Yolyan 1");
             System.out.println(getAddress(complexesMap, complex3));
         } catch (IncorrectFloorException | ComplexNotFoundException e) {
             System.out.println(e.getMessage());
