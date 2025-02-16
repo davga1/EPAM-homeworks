21085690701cf8ba6a64a2b10b75cbd8f88eaeae
davgal <davvv.galstyan@gmail.com>
2025-02-16 12:23:23 +0400

    v1.0

--- src/homework24/Main.java
+++ src/homework24/Main.java
@@ -11,12 +11,15 @@ public class Main {
         }
         System.out.println("Student list:");
         System.out.println(students);
+        //Քանի որ արդյունքները շատ են երկար, կխնդրեմ ամեն մեթոդը ստուգելու համար քոմենթից հանել, հակառակ դեպքում ստուգելը բարդ կլինի
 //        System.out.println(Student.getFilteredStudents(students));
 //        System.out.println(Student.increaseStudentsGrades(students));
 //        System.out.println(Student.getGroupsByBirthYear(students));
 //        System.out.println(Student.getAverageGrade(students));
 //        System.out.println(Student.checkGradesOver45(students));
 //        System.out.println(Student.getStudentWithMinimumGrade(students));
-        System.out.println(Student.getStudentNames(students));
+//        System.out.println(Student.getStudentNames(students));
+//        System.out.println(Student.getGroupOfStudentsByGrades(students));
+//        System.out.println(Student.getYoungestStudent(students));
     }
 }
--- src/homework24/Student.java
+++ src/homework24/Student.java
@@ -14,12 +14,11 @@
 package homework24;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Student {
     private final String name;
     private final int age;
-    private double grade;
+    private final double grade;
     private final String birthYear;
 
     Student(String name, int age, double grade, String birthYear) {
@@ -27,36 +26,6 @@ public class Student {
         this.age = age;
         this.grade = grade;
         this.birthYear = birthYear;
-    }
-
-    public double getGrade() {
-        return grade;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getBirthYear() {
-        return birthYear;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setGrade(double grade) {
-        this.grade = grade;
-    }
-
-    public void upgradeGradeOfStudent(double percent) {
-        setGrade((grade * percent) / 100 + grade);
-    }
-
-    public void checkGrades() {
-        if (grade >= 90) {
-            System.out.println(this);
-        }
     }
 
     public static Student generateRandomStudent() {
@@ -76,57 +45,45 @@ public class Student {
     //Նրանց գնահատականը 60-ից 80-ի սահմաններում է։
     public static List<Student> getFilteredStudents(List<Student> students) {
         System.out.println("Filtered student list:");
-        return students.stream().filter(student -> student.getAge() >= 18 &&
-                        student.getAge() <= 22 && student.getGrade() >= 60 && student.getGrade() <= 80)
-                .toList();
+        return students.stream().filter(student -> student.age >= 18 && student.age <= 22 && student.grade >= 60 && student.grade <= 80).toList();
     }
 
     //Ստեղծել ուսանողների ցուցակ, որոնց գնահատականները բարձրացված են 15%-ով, բայց համոզվել,
-    // որ ոչ մի գնահատական չի գերազանցում 80-ը։
+    //որ ոչ մի գնահատական չի գերազանցում 80-ը։
     public static List<Student> increaseStudentsGrades(List<Student> students) {
         System.out.println("Students grades, increased by 15%");
         return students.stream().map(student -> {
-                    double newGrade = student.grade + (student.grade * 15 / 100);
-                    if (newGrade > 80) {
-                        newGrade = 80;
-                    }
-                    return new Student(student.name, student.age, newGrade, student.birthYear);
-                }
-        ).toList();
+            double newGrade = student.grade + (student.grade * 15 / 100);
+            if (newGrade > 80) {
+                newGrade = 80;
+            }
+            return new Student(student.name, student.age, newGrade, student.birthYear);
+        }).toList();
     }
 
     //Ուսանողներին խմբավորել ծննդյան տարվա հիման վրա և տպել յուրաքանչյուր տարում ուսանողների քանակը։
-
     public static Map<String, Integer> getGroupsByBirthYear(List<Student> students) {
         Map<String, Integer> map = new HashMap<>();
         students.forEach(student -> {
             map.put(student.birthYear, map.getOrDefault(student.birthYear, 0) + 1);
         });
         return map;
-
     }
 
     //Գտնել և հաշվարկել 20 և բարձր տարիք ունեցող ուսանողների միջին գնահատականը։ Արդյունքը մշակել Optional-ով։
     public static OptionalDouble getAverageGrade(List<Student> students) {
-        return students.stream()
-                .filter(student -> student.age >= 20)
-                .mapToDouble(student -> student.grade)
-                .average();
+        return students.stream().filter(student -> student.age >= 20).mapToDouble(student -> student.grade).average();
     }
 
     //Ստուգել՝ արդյո՞ք բոլոր ուսանողների գնահատականները 45-ից բարձր են, և տպել արդյունքը։
     public static boolean checkGradesOver45(List<Student> students) {
-        return students.stream()
-                .allMatch(student -> student.grade > 45);
+        return students.stream().allMatch(student -> student.grade > 45);
     }
 
     //Գտնել ամենացածր գնահատական ունեցող ուսանողին և տպել։
-
     public static Student getStudentWithMinimumGrade(List<Student> students) {
-        Optional<Student> studentWithMinimumGrade = students.stream()
-                .min(Comparator.comparingDouble(student -> student.grade));
-        return studentWithMinimumGrade.map(student -> new Student(student.name, student.age,
-                student.grade, student.birthYear)).orElse(null);
+        Optional<Student> studentWithMinimumGrade = students.stream().min(Comparator.comparingDouble(student -> student.grade));
+        return studentWithMinimumGrade.map(student -> new Student(student.name, student.age, student.grade, student.birthYear)).orElse(null);
     }
 
     //Ստեղծել ուսանողների անունների ստորակետերով բաժանված տող, որոնց անունները դասավորված են այբբենական կարգով, և տպել արդյունքը։
@@ -135,14 +92,20 @@ public class Student {
     }
 
     //Ֆիլտրել և ուսանողներին խմբավորել ըստ գնահատականների (60< , 60 - 70, > 70) և տպել խմբերը։
-    public static Map<String, Student> getGroupOfStudentsByGrades(List<Student> students) {
-        Map<String, List<Student>> map = students.stream().collect(Collectors.groupingBy(student -> {
-            if (student.grade < 60) return "<60";
-            if (student.grade >= 60 && student.grade <= 80) return "60-80";
-            return ">60";
-        }));
-        Map<String, Student> m = new HashMap<>();
-        map.forEach((s, students1) -> m.put(s, students1));
+    public static Map<String, List<Student>> getGroupOfStudentsByGrades(List<Student> students) {
+        List<Student> a = students.stream().filter(student -> student.grade < 60).toList();
+        List<Student> b = students.stream().filter(student -> student.grade >= 60 && student.grade <= 70).toList();
+        List<Student> c = students.stream().filter(student -> student.grade > 80).toList();
+        Map<String, List<Student>> groupedList = new HashMap<>();
+        groupedList.put("\n60<", a);
+        groupedList.put("\n60-70", b);
+        groupedList.put("\n70>", c);
+        return groupedList;
+    }
+
+    //Գտնել ամենաերիտասարդ ուսանողին
+    public static Student getYoungestStudent(List<Student> students) {
+        return students.stream().min(Comparator.comparingInt(student -> student.age)).get();
     }
 
     public String toString() {
