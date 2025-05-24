ebacbe473c9f312cf4b070edf6627c6fc3054d2c
davgal <davvv.galstyan@gmail.com>
2025-05-24 11:00:40 +0400

    tokens

---
+++ src/test/java/gorest_homework/AccessToken.java
@@ -0,0 +1,5 @@
+package gorest_homework;
+
+public class AccessToken {
+    public static final String accessToken = System.getenv("GOREST_TOKEN");
+}
---
+++ src/test/java/gorest_homework2/AccessToken.java
@@ -0,0 +1,5 @@
+package gorest_homework2;
+
+public class AccessToken {
+    public static final String accessToken = System.getenv("GOREST_TOKEN");
+}
